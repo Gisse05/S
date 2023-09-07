b818353d844d0351c51fdae9520e94e35d4ab7b6
Gisse05 <143531332+Gisse05@users.noreply.github.com>
2023-09-07 08:18:54 -0600

    Add files via upload

---
+++ TS18_SGCH1361923.py
@@ -0,0 +1,9 @@
+
+
+texto1= input("ingrese su nombre")
+texto2= input("ingrese su edad")
+texto3= input("ingrese su carrera")
+texto4= input("ingrese su carne")
+
+print("Mi segundo Programa")
+print("mi nombre es" + texto1 + "tengo" + texto2 + "años y estudio la carrera de" + texto3 + "mi numero de carne" + texto4 )
